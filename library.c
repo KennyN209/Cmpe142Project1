be70eca102a72ba16d4fffe702eac44704e917fb
Enrique <enrique.alvarez01@sjsu.edu>
2019-03-22 13:28:05 -0700

    *Starting main -EA *Pushing first code to repository - EA ------------------------------------------ *built on main and added some more global vars - EA -------------------------------------------------------- *Added PromptPrintScreen() and ErrorScreen() - EA *Added <unistd.h> library - EA

--- library.c
+++ library.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h> //output data from a program by directly using a system call
+
+
 
 //GLOBAL VARIABLES
 int BufferSize = 256; //2 to the 8th power?
 int batch = 0; //to check if input is a batch file or not
 char *path;
+char Error[126] = "Something has gone wrong\n"; //C string array for error message array size of 126
+
+
+
+
+
+
+//Main
 int main(int argc, char* argv[])
 {
 FILE *file = NULL; //create a point of FILE type that is NULLED
@@ -32,4 +43,18 @@ if (argc == 1)
     {
         ErrorScreen();
     }
+}
+
+
+//Functons&Auxillory
+
+void PromptPrintScreen()
+{
+    write(STDERR_FILENO, "Poop> ", strlen("Poop> ")); //STDERR is file descriptor likely 0 and poop for the buffer
+}
+
+void ErrorScreen()
+{
+    write(STDERR_FILENO, Error, strlen(Error)); //Will write Error message using the length of it as buffer
+    exit(1);
 }
