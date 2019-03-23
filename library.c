3c2c8d83af1dabfdf960413a03da134b68327286
KennyN209 <48034019+KennyN209@users.noreply.github.com>
2019-03-22 19:42:41 -0700

    4

--- library.c
+++ library.c
@@ -141,7 +141,7 @@ int Process(char *argc1[])
       int a;
       int i;
       
-      if(strstr(space, ">")!=NULL
+      if(strstr(space, ">")!=NULL)
          {
              i = 0;
              char* shift[sizeof(char)*256];
@@ -160,6 +160,131 @@ int Process(char *argc1[])
              int z = 0;
              char* argc1[sizeof(space)];
              argc1[0] = strtok(space);
+             while(argc1[z]!=NULL)
+             {
+                 z++;
+                 argc1[z] = strtok(NULL);
+             }
+             if(z>2)
+             {
+                 write(STDERR_FILENO, Error, strlen(Error));
+                 exit(0);
+             }
+             int b = 0;
+             char* size1[sizeof(argc1[1])];
+             size1[0] = strtok(argc1[1]);sw
+             while(size1[b]!=NULL)
+             {
+                 b++;
+                 size1[b]=strtok(NULL);
+             }
+             char *switch = strdup(size1[0]);
+             stdout_copy = dup(1);
+             int away = open(switch,O_WRONLY|O_CREAT|O_TRUNC,0666);
+             int err = open(switch,O|WRONLY|O_CREAT|O_TRUNC,0666);
+             fflush(stdout);
+             dup2(away, STDOUT_FILENO);
+             dup2(away,STDERR_FILENO);
+             close(away);
+             CLOSED = 1;
+             if(away == -1 || err == -1 || b > 1 || z > 2)
+             {
+                 write(STDERR_FILENO, Error, strlen(Error));
+                 exit(0);
+             }
+             argc1[z+1] = NULL;
+             size1[b+1] = NULL;
+             strcpy(space, argc1[0]);
+         }
+         
+         if(space[0] != '\0' && space[0] != '\n')
+         {
+             char *ord[sizeof(space)];
+             int c = 0;
+             ord[0] = strtok(space);
+             while(ord[c]!=NULL)
+             {
+                 c++;
+                 ord[c] = strtok(NULL);
+             }
+             ord[c+1} = NULL;
+                 if(strcmp(ord[0])
+                    {
+                        if(c==2)
+                        {
+                            if(chdir(odr[1])!=0)
+                            {
+                                write(STDERR_FILENO, Error, strlen(Error));
+                            }
+                        }
+                        else
+                        {
+                            write(STDERR_FILENO, Error, strlen(Error));
+                        }
+                    }
+                    else if(strcmp(ord[0], "path") == 0)
+                    {
+                        modify = 1;
+                        if(c == 2)
+                        {
+                            ProcessEmpty=0;
+                            path = strdup(ord[1]);
+                            if(path[strlen(path)-1]!='/')
+                            {
+                                strcat(path, "/");
+                            }
+                        }
+                        else if(c == 1)
+                        {
+                            ProcessEmpty = 1;
+                        }
+                        else
+                        {
+                            ProcessEmpty = 0;
+                            for(int a = 1; i<a; a++)
+                            {
+                                char *tem = strdup(ord[i]);
+                                if(tem[strlen(tem)-1]!='/')
+                                {
+                                    strcat(tem,"/");
+                                }
+                                strcpy(multi[a-1], tem);
+                                mult++;
+                            }
+                        }
+                        else if(strcmp(ord[0], "exit") == 0)
+                        {
+                            if(c==1)
+                            {
+                                exit(0);
+                            }
+                            else
+                            {
+                                write(STDERR_FILENO, Error, strlen(Error));
+                            }
+                        }
+                        else
+                        {
+                            if(ProcessEmpty == 1)
+                            {
+                                write(STDERR_FILENO, Error, strlen(Error));
+                            }
+                            else
+                            {
+                                a = newProcess(ord);
+                            }
+                        }
+                    }
+                    if(closed == 1)
+                    {
+                        dup2(stdout_copy, 1);
+                        close(stdout_copy);
+                    }
+                    return a;
+                    
+ }
+             
+             
              
               
         
